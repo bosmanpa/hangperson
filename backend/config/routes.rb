87bdac1bbbd497a391495857181e1e038b8a578d
benjewman <bennyadave@gmail.com>
2019-12-04 14:40:03 -0600

    broken

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :games, only: [:index, :create]
-  resources :phrases, only: [:index, :show, :destroy, :create]
+  resources :games, only: [:index, :create, :destroy]
+  resources :phrases, only: [:index, :show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :players, only: [:index, :show, :create]
+  resources :players, only: [:index, :show, :create, :destroy]
 
 end
